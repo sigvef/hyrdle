a2e86edd6422343213e138cbffddf72fa3643d1c
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2022-05-15 21:09:47 +0200

    Adjust day offset

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import { useRef } from "react";
 
 export function useDay() {
-  const dayOffset = 19125 - 4;
+  const dayOffset = 19125 - 6;
   /* Needs to be a ref so we don't accidentally switch days while playing. */
   const currentDay = useRef((+new Date() / 1000 / 60 / 60 / 24) | 0);
   const todaysIndex = currentDay.current - dayOffset;
