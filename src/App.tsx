42b61b6a09a509c0ab9bf8d8ea1bef1264e73e8d
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2022-05-15 17:01:24 +0200

    Switch from next to vite

---
+++ src/App.tsx
@@ -0,0 +1,365 @@
+import { useEffect, useRef, useState } from "react";
+import { GameMap, getBoundsFromCircle, LatLngLiteral } from "./GameMap";
+import styles from "./Home.module.css";
+import vehicles from "./data.json";
+import { useGoogleMaps } from "./googlemaps";
+import { useDay } from "./utils";
+
+import seedrandom from "seedrandom";
+
+const App = () => {
+  const { dayOffset, currentDay, todaysIndex } = useDay();
+
+  const { maps } = useGoogleMaps();
+
+  const [hasLoaded, setHasLoaded] = useState(false);
+
+  const makeLocalStorageKey = (key: string) =>
+    `hyrdle.xyz:${todaysIndex}:${key}`;
+
+  const [hasShared, setHasShared] = useState(false);
+  const mapRef = useRef<any>(null);
+  const currentVehicle = vehicles[todaysIndex];
+  const answerPoint = {
+    lat: currentVehicle.point[1],
+    lng: currentVehicle.point[0],
+  };
+
+  const [markers, setMarkers] = useState<
+    {
+      lat: number;
+      lng: number;
+    }[]
+  >([]);
+  const [marker, setMarker] = useState<{
+    lat: number;
+    lng: number;
+  } | null>(null);
+  const circles = [
+    {
+      center: { lat: 59.95, lng: 10.5 },
+      radius: 600000,
+    },
+  ];
+  if (maps) {
+    const random = seedrandom(todaysIndex + "circles");
+    for (let i = 0; i < 4; i++) {
+      const previousCircle = circles[circles.length - 1];
+      /* Find random point in circle with radius away from real answer. That point is now the circle's new midpoint. */
+      const newRadius = Math.pow(previousCircle.radius, 0.8);
+      const r = newRadius * Math.sqrt(random() * 0.9);
+      const theta = random() * 360;
+      const latLng = maps.geometry.spherical
+        .computeOffset(answerPoint, r, theta)
+        .toJSON();
+      circles.push({ center: latLng, radius: newRadius });
+    }
+  }
+
+  const [gameState, setGameState] = useState<"playing" | "win" | "lose">(
+    "playing"
+  );
+  const level = markers.length;
+
+  const circle = circles[level];
+
+  const fitMapToCircleBounds = ({
+    center,
+    radius,
+  }: {
+    center: LatLngLiteral;
+    radius: number;
+  }) => {
+    const bounds = getBoundsFromCircle({ center, radius });
+    mapRef.current.fitBounds(bounds, 16);
+  };
+
+  const distance =
+    (marker && answerPoint
+      ? window.google?.maps.geometry.spherical.computeDistanceBetween(
+          answerPoint,
+          marker
+        )
+      : -1) | 0;
+
+  const endGame = (state: "win" | "lose", marker: LatLngLiteral) => {
+    setGameState(state);
+    const dLng = marker.lng - answerPoint.lng;
+    const dLat = marker.lat - answerPoint.lat;
+    const bounds = {
+      north: Math.max(answerPoint.lat - dLat, answerPoint.lat + dLat),
+      south: Math.min(answerPoint.lat - dLat, answerPoint.lat + dLat),
+      east: Math.max(answerPoint.lng - dLng, answerPoint.lng + dLng),
+      west: Math.min(answerPoint.lng - dLng, answerPoint.lng + dLng),
+    };
+    if (mapRef.current) {
+      mapRef.current?.fitBounds(bounds, 32);
+    }
+  };
+
+  /* Load previous state from local storage */
+  useEffect(() => {
+    if (!maps) {
+      return;
+    }
+    setHasLoaded(true);
+    const markers = JSON.parse(
+      localStorage.getItem(makeLocalStorageKey("markers")) || "[]"
+    );
+    if (markers.length) {
+      setMarkers(markers);
+      if (markers.length < 5) {
+        fitMapToCircleBounds(circles[markers.length]);
+      }
+      const value = evaluateWinCondition(
+        answerPoint,
+        markers[markers.length - 1],
+        5,
+        maps
+      );
+      if (value) {
+        setMarker(markers[markers.length - 1]);
+        endGame(value, markers[markers.length - 1]);
+      }
+    }
+  }, [maps]);
+
+  const isGameDone = gameState === "win" || gameState === "lose";
+
+  const evaluateWinCondition = (
+    pointA: LatLngLiteral,
+    pointB: LatLngLiteral,
+    level: number,
+    maps: typeof google.maps
+  ) => {
+    const guessedDistance = maps.geometry.spherical.computeDistanceBetween(
+      pointA,
+      pointB
+    );
+    if (guessedDistance < 50) {
+      return "win";
+    }
+    if (level > 4) {
+      return "lose";
+    }
+    return "";
+  };
+
+  const makeGuess = (marker: LatLngLiteral) => {
+    const newLevel = level + 1;
+
+    //@ts-expect-error
+    const geometry = maps.geometry;
+
+    if (marker !== null && markers.length < 5) {
+      const newMarkers = [...markers, marker];
+      localStorage.setItem(
+        makeLocalStorageKey("markers"),
+        JSON.stringify(newMarkers)
+      );
+      setMarkers(newMarkers);
+    }
+    if (!maps) {
+      return;
+    }
+    const endGameState = evaluateWinCondition(
+      answerPoint,
+      marker,
+      newLevel,
+      maps
+    );
+    if (endGameState) {
+      endGame(endGameState, marker);
+      return;
+    }
+    setMarker(null);
+    fitMapToCircleBounds(circles[newLevel]);
+  };
+
+  return (
+    <div
+      style={{
+        background: "#1c5dff",
+        minHeight: "100vh",
+        color: "white",
+        fontSize: "white",
+        lineHeight: "1.5",
+        fontFamily:
+          '-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif',
+      }}
+    >
+      <div
+        style={{
+          maxWidth: 640 + 16 * 2,
+          width: "100%",
+          margin: "0 auto",
+          background: "#1c5dff",
+          padding: 16,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <div
+          style={{
+            display: "block",
+            marginTop: 32,
+            marginBottom: 32,
+          }}
+        >
+          <img width={168} height={26} src="/hyrdle.svg" alt="Hyrdle" />
+        </div>
+        <div
+          style={{
+            transition: "all 0.15s ease-out",
+            opacity: hasLoaded ? 1 : 0,
+            transform: `translate3d(0px, ${hasLoaded ? 0 : 8}px, 0px)`,
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          {!isGameDone && (
+            <div style={{ marginBottom: 32 }}>
+              Where is today&apos;s car? Find it on the map!
+            </div>
+          )}
+          {!isGameDone && (
+            <div
+              style={{
+                boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.16)",
+                width: "100%",
+                aspectRatio: (16 / 9).toString(),
+                borderRadius: 16,
+                background: `#888 url(${currentVehicle.image.replace(
+                  "__cover_640x480.jpg",
+                  "__cover_1920x1080.jpg"
+                )}) no-repeat center / contain`,
+              }}
+            />
+          )}
+
+          {!isGameDone && (
+            <div style={{ marginTop: 32 }}>
+              The car is somewhere within the blue circle. This is guess number{" "}
+              {level + 1}. You have {5 - level} attempts remaining.
+            </div>
+          )}
+
+          <div
+            style={{
+              width: "100%",
+              boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.06)",
+              height: 480,
+              marginTop: 32,
+              borderRadius: 16,
+              overflow: "hidden",
+              position: "relative",
+            }}
+          >
+            <GameMap
+              onMap={(map) => (mapRef.current = map)}
+              circle={isGameDone ? null : circle}
+              marker={marker || undefined}
+              carMarker={isGameDone ? answerPoint : undefined}
+              onChange={(latLng) => {
+                if (!isGameDone) {
+                  setMarker(latLng);
+                }
+              }}
+            />
+          </div>
+          {!isGameDone && marker && (
+            <button
+              className={styles.guessButton}
+              style={{ marginTop: -64 - 16, marginBottom: 32 }}
+              onClick={() => makeGuess(marker)}
+            >
+              Make a guess
+            </button>
+          )}
+          {isGameDone && (
+            <div
+              style={{
+                marginTop: 32,
+                display: "flex",
+                alignItems: "center",
+                flexDirection: "column",
+              }}
+            >
+              <div style={{ marginBottom: 32 }}>
+                You guessed <strong>{distance}m</strong> away from the car.
+              </div>
+              <button
+                className={styles.guessButton}
+                onClick={() => {
+                  navigator.clipboard.writeText(
+                    `Hyrdle #${todaysIndex}
+                ${[...new Array(5)]
+                  .map((_, i) => {
+                    if (i === level - 1) {
+                      return gameState === "lose" ? "🟥" : "🟩";
+                    }
+                    if (i <= level - 1) {
+                      return "🟥";
+                    }
+                    return "⬛";
+                  })
+                  .join("")} ${distance}m
+                https://hyrdle.xyz`
+                      .split("\n")
+                      .map((x) => x.trim())
+                      .join("\n")
+                  );
+                  setHasShared(true);
+                }}
+              >
+                {hasShared ? "Copied!" : "Share"}
+              </button>
+              <div
+                style={{
+                  marginTop: 32,
+                  fontWeight: "bold",
+                  background: "#ffffff",
+                  padding: "16px 32px",
+                  borderRadius: 16,
+                  color: "#000130",
+                }}
+              >
+                <div>Hyrdle #{todaysIndex}</div>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  {[...new Array(5)].map((_, i) => (
+                    <span key={i} style={{ padding: 2 }}>
+                      {(() => {
+                        if (i === level - 1) {
+                          return gameState === "lose" ? "🟥" : "🟩";
+                        }
+                        if (i <= level - 1) {
+                          return "🟥";
+                        }
+                        return "⬛";
+                      })()}
+                    </span>
+                  ))}
+                  <strong style={{ paddingLeft: 6 }}>{distance}m</strong>
+                </div>
+                <div>https://hyrdle.xyz</div>
+              </div>
+
+              <a
+                href={`https://link.hyre.no/h?communityvehiclestation=${currentVehicle.id}`}
+                style={{ marginTop: 32, textDecoration: "underline" }}
+              >
+                Open in the Hyre app.
+              </a>
+            </div>
+          )}
+        </div>
+        <div style={{ height: 128 }} />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/GameMap.tsx
@@ -0,0 +1,226 @@
+import { useEffect, useId, useRef, useState } from "react";
+import { useGoogleMaps } from "./googlemaps";
+
+export interface LatLngLiteral {
+  lat: number;
+  lng: number;
+}
+
+interface GameMapProps {
+  marker?: LatLngLiteral;
+  carMarker?: LatLngLiteral;
+  circle?: { radius: number; center: LatLngLiteral } | null;
+  onChange?: (latLng: LatLngLiteral) => void;
+  onMap: (map: google.maps.Map) => void;
+}
+
+const lineSymbol = {
+  path: "M 0,-1 0,1",
+  strokeColor: "#000130",
+  strokeOpacity: 0.8,
+  scale: 4,
+};
+
+interface CircleLiteral {
+  center: LatLngLiteral;
+  radius: number;
+}
+
+export const getBoundsFromCircle = ({ center, radius }: CircleLiteral) => {
+  const maps = window.google.maps;
+  return {
+    north: maps.geometry.spherical.computeOffset(center, radius, 0).lat(),
+    east: maps.geometry.spherical.computeOffset(center, radius, 90).lng(),
+    south: maps.geometry.spherical.computeOffset(center, radius, 180).lat(),
+    west: maps.geometry.spherical.computeOffset(center, radius, 270).lng(),
+  };
+};
+
+export const GameMap = (props: GameMapProps) => {
+  const { mapsPromise } = useGoogleMaps();
+  const id = useId();
+  const circleObj = useRef<google.maps.Circle | null>();
+  const carMarkerObj = useRef<google.maps.Marker | null>();
+  const markerObj = useRef<google.maps.Marker | null>();
+  const polylineObj = useRef<google.maps.Polyline | null>();
+  const [mapPromise] = useState<Promise<google.maps.Map>>(
+    () =>
+      new Promise(async (resolve) => {
+        const map = new (await mapsPromise).Map(document.getElementById(id)!, {
+          center: { lat: 59.95, lng: 10.5 },
+          zoom: 5,
+          gestureHandling: "greedy",
+        });
+        props.onMap(map);
+        resolve(map);
+      })
+  );
+
+  useEffect(() => {
+    const circle = props.circle;
+    if (circle) {
+      mapsPromise.then((maps) => {
+        mapPromise.then((map) => {
+          const obj =
+            circleObj.current ||
+            new maps.Circle({
+              strokeColor: "#1c5dff",
+              strokeOpacity: 0.8,
+              strokeWeight: 2,
+              fillColor: "#1c5dff",
+              fillOpacity: 0.1,
+              zIndex: 1,
+            });
+          if (!circleObj.current) {
+            obj.addListener("click", (e: any) =>
+              props.onChange?.(e.latLng.toJSON())
+            );
+          }
+          circleObj.current = obj;
+          obj.setCenter(circle.center);
+          obj.setRadius(circle.radius);
+          obj.setMap(map);
+        });
+      });
+    } else {
+      circleObj.current?.setMap(null);
+    }
+  }, [props.circle, mapsPromise, mapPromise]);
+
+  useEffect(() => {
+    const carMarker = props.carMarker;
+    if (carMarker) {
+      mapsPromise.then((maps) => {
+        mapPromise.then((map) => {
+          const obj = (carMarkerObj.current =
+            carMarkerObj.current || new maps.Marker({ zIndex: 9 }));
+          obj.setPosition(carMarker);
+          obj.setMap(map);
+        });
+      });
+    }
+  }, [props.carMarker, mapsPromise, mapPromise]);
+
+  useEffect(() => {
+    const marker = props.marker;
+    if (marker) {
+      mapsPromise.then((maps) => {
+        mapPromise.then((map) => {
+          const obj =
+            markerObj.current ||
+            new maps.Marker({ zIndex: 10, draggable: true });
+          if (!markerObj.current) {
+            obj.addListener("dragEnd", (e: any) =>
+              props.onChange?.(e.latLng.toJSON())
+            );
+          }
+          markerObj.current = obj;
+          obj.setPosition(marker);
+          obj.setMap(map);
+        });
+      });
+    } else {
+      markerObj.current?.setMap(null);
+    }
+  }, [props.marker, mapsPromise, mapPromise]);
+
+  useEffect(() => {
+    if (props.marker && props.carMarker) {
+      mapsPromise.then((maps) => {
+        mapPromise.then((map) => {
+          const obj =
+            polylineObj.current ||
+            new maps.Polyline({
+              path: [props.carMarker!, props.marker!],
+              clickable: false,
+              draggable: false,
+              editable: false,
+              geodesic: true,
+              strokeOpacity: 0,
+              icons: [
+                {
+                  icon: lineSymbol,
+                  offset: "0",
+                  repeat: "20px",
+                },
+              ],
+            });
+          polylineObj.current = obj;
+          obj.setMap(map);
+        });
+      });
+    } else {
+      polylineObj.current?.setMap(null);
+    }
+  }, [props.marker, props.carMarker, mapsPromise, mapPromise]);
+
+  return (
+    <>
+      <div
+        id={id}
+        style={{ width: "100%", height: "100%", background: "#888" }}
+      ></div>
+      {/*
+    <GoogleMap
+      mapContainerStyle={{ width: "100%", height: "100%" }}
+      onLoad={(map) => {
+        console.log("le load", map);
+        map.setCenter({ lat: 59.95, lng: 10.5 });
+        map.setZoom(5);
+        props.onMapMounted(map);
+      }}
+      options={{ gestureHandling: "greedy" }}
+      onClick={(e) => {
+        e.latLng && props.onChange?.(e.latLng.toJSON());
+      }}
+    >
+      {props.carMarker && <Marker position={props.carMarker} />}
+      {!!(props.carMarker && props.marker) && (
+        <>
+          <Polyline
+            path={[props.carMarker, props.marker]}
+            options={{
+              clickable: false,
+              draggable: false,
+              editable: false,
+              geodesic: true,
+              strokeOpacity: 0,
+              icons: [
+                {
+                  icon: lineSymbol,
+                  offset: "0",
+                  repeat: "20px",
+                },
+              ],
+            }}
+          />
+        </>
+      )}
+      {props.marker && (
+        <Marker
+          position={props.marker}
+          draggable={!props.carMarker}
+          onDragEnd={(e) => e.latLng && props.onChange?.(e.latLng.toJSON())}
+        />
+      )}
+      {props.circle && (
+        <Circle
+          center={props.circle.center}
+          options={{
+            strokeColor: "#1c5dff",
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: "#1c5dff",
+            fillOpacity: 0.1,
+          }}
+          radius={props.circle.radius}
+          onClick={(e) => {
+            e.latLng && props.onChange?.(e.latLng.toJSON());
+          }}
+        />
+      )}
+    </GoogleMap>
+    */}
+    </>
+  );
+};
